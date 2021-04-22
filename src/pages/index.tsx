8bf69434dd44f343f38450ad5d7ac7ed3aa05ba8
jocimarjsc <jocimarjsc@yahoo.com.br>
2021-04-22 08:15:58 -0300

    initial project

---
+++ src/pages/index.tsx
@@ -0,0 +1,96 @@
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+import styles from "./home.module.scss";
+
+interface Episodes {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string
+  publishedAt: string;
+}
+
+interface HomeProps {
+  latesEpisodes: Episodes[];
+  allEspisodes: Episodes[];
+}
+
+export default function Home({ latesEpisodes, allEspisodes }: HomeProps) {
+  return (
+    <div className={styles.homePage}>
+      <section className={styles.latesEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latesEpisodes.map(episode => {
+            return(
+              <li key={episode.id}>
+                <img src={episode.thumbnail} alt={episode.title} />
+
+                <div className={styles.episodeDetails}>
+                  <a href="#">{episode.title}</a>
+
+                  <p>{episode.members}</p>
+
+                  <span>{episode.publishedAt}</span>
+
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio"/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEspisodes}>
+
+      </section>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("/episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _orderr: "desc"
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  })
+
+  const latesEpisodes = episodes.slice(0, 2);
+  const allEspisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latesEpisodes,
+      allEspisodes
+    }
+  }
+}
